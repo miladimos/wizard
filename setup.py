410ac1c9e2021df1951c63ebd910c63aa6104833
alimohammad0816 <alimohammad0816@gmail.com>
2021-12-02 21:42:45 +0330

    minor changes

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from setuptools import setup
 
 setup(
     name='wizard',
-    version='0.1.0',
+    version='0.0.1',
     description='a python package for install simply linux apps',
-    author=['miladimos', ''],
-    author_email=['miladimos@outlook.com', ''],
+    author=['miladimos', 'alimohammad0816'],
+    author_email=['miladimos@outlook.com', 'alimohammad0816@gmail.com'],
     keywords=['python3', 'wizard', 'wizard package', 'linux installer'],
     packages=['wizard'],
     install_requires=[
         'importlib',
         'requests',        
     ],
-    python_required=[">=3.7"]
+    python_required=[">=3.6"]
 )
